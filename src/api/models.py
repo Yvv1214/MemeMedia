9385e881b39e6a68736f6af92ddde812cec665ea
Yaira V <39809411+Yvv1214@users.noreply.github.com>
2023-10-06 18:05:32 +0000

    creating models

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,5 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -10,6 +12,11 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     phone = db.Column(db.String(10), unique=True, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=True)
+    user_comment = db.relationship("Comments", backref = 'user')
+    user_following = db.relationship('Following', backref = 'user')
+    user_followers = db.relationship('Followers', backref = 'user')
+    user_blocking = db.relationship('Blocked', backref = 'user')
+    user_posts = db.relationship('Posts', backref = 'user')
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -24,4 +31,35 @@ class User(db.Model):
             "phone": self.phone
             # do not serialize the password, its a security breach
         }
+    
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    comment =db.Column(db.String(300), unique=False, nullable=True)
+
+
+class Following(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    your_following = db.Column(db.Boolean(), unique=False, nullable=True)
+
+
+class Followers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    your_followers = db.Column(db.Boolean(), unique=False, nullable=True)
+
+
+class Blocked(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    blocked_user = db.Column(db.Boolean(), unique=False, nullable=True)
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    media = db.Column(db.Boolean(), unique=False, nullable=True)
+    description = db.Columnn(db.String(300), nullable=True)
+    hashtag = db.Column(db.string(200), nullable=True)
+
+
+
+
     
